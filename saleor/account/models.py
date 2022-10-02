f73692f540c7a051c5b4fd4afce534440894039a
nguyenduclong1147 <69979714+nguyenduclong1147@users.noreply.github.com>
2022-10-02 21:14:48 +0700

    add Account - model - Address - phone

--- saleor/account/models.py
+++ saleor/account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from .validators import validate_possible_number
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+class PossiblePhoneNumberField(PhoneNumberField):
+    """Less strict field for phone numbers written to database."""
+
+    default_validators = [validate_possible_number]
+
+class Address(models.Model):
+    phone = PossiblePhoneNumberField(blank=True, default="", db_index=True)
--- saleor/settings.py
+++ saleor/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'saleor.account',
+    'saleor.graphqlApi'
 ]
 
 MIDDLEWARE = [
