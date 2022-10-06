60e3f85f7d521b4a4da83e89062f439397d55c42
nguyenduclong1147 <69979714+nguyenduclong1147@users.noreply.github.com>
2022-10-06 15:37:34 +0700

    learn view

--- saleor/settings.py
+++ saleor/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'saleor.account',
-    'saleor.graphqlApi'
+    'saleor.graphqlApi',
+    "graphene_django"
 ]
 
 MIDDLEWARE = [
--- saleor/urls.py
+++ saleor/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf.urls import include
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 
+
+from .graphqlApi.api import schema
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r"^graphql/$", csrf_exempt(GraphQLView.as_view(schema=schema)), name="api")
 ]
